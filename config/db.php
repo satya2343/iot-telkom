2e631dce9f8065fa5903ba314c69006c6dda847a
satya2343 <satya24@student.uns.ac.id>
2017-07-04 23:22:08 +0700

    setting database dan layout awal

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=iot_telkom',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
